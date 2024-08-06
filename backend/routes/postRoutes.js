5b47730cdc56c7bf72c17d0b3fb348901f64a1e0
Shashank <shashank78680@gmail.com>
2024-08-07 00:20:21 +0530

    get posts by username

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPost, getAllPosts, deletePost, likeOrUnlikePost, commentOnPost, getAllCommentsOnAPost, getAllLikedPostsByAnUser, getAllPostsOfFollowedUsers } from '../controllers/postController.js';
+import { createPost, getAllPosts, deletePost, likeOrUnlikePost, commentOnPost, getAllCommentsOnAPost, getAllLikedPostsByAnUser, getAllPostsOfFollowedUsers, getUserPosts } from '../controllers/postController.js';
 import { protectRoutewithJwt } from '../middlewares/protectRouteWithJwt.js';
 
 const postRoutes = express.Router();
@@ -12,5 +12,6 @@ postRoutes.post('/comment/:id', protectRoutewithJwt, commentOnPost);
 postRoutes.get('/comments/:id', protectRoutewithJwt, getAllCommentsOnAPost);
 postRoutes.get('/likes/:id', protectRoutewithJwt, getAllLikedPostsByAnUser);
 postRoutes.get('/following', protectRoutewithJwt, getAllPostsOfFollowedUsers);
+postRoutes.get('/user/:username', protectRoutewithJwt, getUserPosts);
 
 export default postRoutes;
