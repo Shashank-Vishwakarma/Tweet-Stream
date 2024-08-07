2a8ee22f80ba4bc9b4e2e53f30954966cab96847
Shashank <shashank78680@gmail.com>
2024-08-07 17:06:37 +0530

    home page and routing done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
-import React from "react"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import Notification from "./pages/notification/Notification";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
-    <div>
-      App
+    <div className='flex max-w-6xl mx-auto'>
+      <BrowserRouter>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/notifications' element={<Notification />} />
+          <Route path='/profile/:username' element={<Profile />} />
+        </Routes>
+        <RightPanel />
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App
