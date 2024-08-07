aafec2b70c1387555dab4d5c376280c877c677ec
Shashank <shashank78680@gmail.com>
2024-08-07 12:17:02 +0530

    delete notifications route done

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -26,7 +26,15 @@ export const getNotifications = async (req, res) => {
 
 export const deleteNotifications = async (req, res) => {
     try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(400).json({ error: "User not found" });
+        }
 
+        await Notification.deleteMany({ to: userId });
+
+        res.status(200).json({ message: "Notifications deleted successfully" });
     } catch (error) {
         console.log(`Error in getNotifications: ${error.message}`);
         res.status(500).json({ error: "Internal server error" });
