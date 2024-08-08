e77d286737d541876120655bbb7e9292108d6fa3
Shashank <shashank78680@gmail.com>
2024-08-08 23:00:35 +0530

    config added in queryclient

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,13 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AuthContextProvider from './context/authContext.jsx';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
